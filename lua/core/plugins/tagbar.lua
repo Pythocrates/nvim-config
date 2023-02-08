ee41c9af0044ab9536dbe4931e2b76816a7a83a4
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2023-02-09 00:29:12 +0100

    Added Tagbar plugin.

---
+++ lua/core/plugins/tagbar.lua
@@ -0,0 +1,11 @@
+-- Outline using tags (works standalone). Best structured.
+-- Still: SymbolsOutline is based on LSP.
+
+local M = {
+    'majutsushi/tagbar',
+    config = function()
+        vim.g.tagbar_width = 60
+    end
+}
+
+return M
