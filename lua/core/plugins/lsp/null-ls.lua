a8b28f09f38b0cdb9962e2b13ae237143f598d12
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2023-02-09 00:29:12 +0100

    Prepare for isort.

--- lua/core/plugins/lsp/null-ls.lua
+++ lua/core/plugins/lsp/null-ls.lua
@@ -10,6 +10,7 @@ nls.setup({
     }),
     nls.builtins.formatting.terraform_fmt,
     nls.builtins.formatting.black,
+    -- nls.builtins.formatting.isort,
     nls.builtins.formatting.taplo, -- for TOML
     nls.builtins.formatting.goimports,
     nls.builtins.formatting.gofumpt,
