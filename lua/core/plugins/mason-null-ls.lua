f07b5edd28efdeed71dd923766fcd267eb498bde
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2023-02-15 19:43:33 +0100

    Remove mason-installed black, use local one.

--- lua/core/plugins/mason-null-ls.lua
+++ lua/core/plugins/mason-null-ls.lua
@@ -2,7 +2,7 @@ local M = {
   "jayp0521/mason-null-ls.nvim",
   config = function()
     require("mason-null-ls").setup({
-      automatic_installation = true,
+      automatic_installation = { exclude = { "black" } },
       automatic_setup = false,
       ensure_installed = nil,
     })
